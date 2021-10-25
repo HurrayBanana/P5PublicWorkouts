1105c8a5102732764b370ff59e2f7a7b840d510d
hurraybanana <HurrayBanana@outlook.com>
2021-10-25 20:34:46 +0100

    try out

--- Shaders/sketch.js
+++ Shaders/sketch.js
@@ -4,24 +4,29 @@ let theShader;
 function preload(){
   // load the shader
   theShader = loadShader(
-    'https://raw.githubusercontent.com/HurrayBanana/filehosting/main/Shaders/basic.vert', 
-    'https://raw.githubusercontent.com/HurrayBanana/filehosting/main/Shaders/basic.frag');
+    'https://raw.githubusercontent.com/HurrayBanana/P5PublicWorkouts/main/Shaders/basic.vert', 
+    'https://raw.githubusercontent.com/HurrayBanana/P5PublicWorkouts/main/Shaders/basic.frag');
+    
 }
 
 function setup() {
+  // disables scaling for retina screens which can create inconsistent scaling between displays
+  pixelDensity(1);
+  
   // shaders require WEBGL mode to work
-  createCanvas(400, 400, WEBGL);
+  createCanvas(windowWidth, windowHeight, WEBGL);
   noStroke();
 }
 
-function draw() {
+function draw() {  
+  // send resolution of sketch into shader
+  theShader.setUniform('u_resolution', [width, height]);
+
   // shader() sets the active shader with our shader
   shader(theShader);
+
   // rect gives us some geometry on the screen
-  rect(0,0,100,200);
- 
-  // print out the framerate
-  //print(frameRate());
+  rect(0,0,width, height);
 }
 
 function windowResized(){
